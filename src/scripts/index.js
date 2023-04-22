6b2e04dc1bb70d441f41cec9f0f78260284c37f1
Kirill Shlenkin <Shlyonkin.K@yandex.ru>
2023-04-22 22:11:25 +0300

    footer&rightsidemenu

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,15 @@
 import "../pages/index.css";
 
+const headerButton = document.querySelector('.header__button');
+const rightsideMenu = document.querySelector('.rightside-menu');
+const rightsideMenuClose = document.querySelector('.rightside-menu--close');
+
+headerButton.addEventListener('click', () => {
+    rightsideMenu.classList.remove('rightside-menu--close');
+});
+
+rightsideMenuClose.addEventListener('click', () => {
+    rightsideMenu.classList.add('rightside-menu--close');
+});
+
 console.log("Сборка работает!!!!")
